f1850eb6ddb4d667161f22ba76a5d7b26d602e42
Pablo Arias <JuanPablo.Arias@team.neustar>
2021-05-30 09:37:08 -0600

    estypidez

--- ssg/content.py
+++ ssg/content.py
@@ -10,10 +10,10 @@ class Content(Mapping):
     @classmethod
     def load(cls, string):
         _, fm, content = cls.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
-        return cls(cls.metadata, content)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
-        self.data.add({"content", content})
+        self.data["content"] = content
 
