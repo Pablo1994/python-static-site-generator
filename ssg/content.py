7c35590fba9039acb2d8c2069196df94a702cebb
Pablo Arias <JuanPablo.Arias@team.neustar>
2021-05-30 09:32:38 -0600

    content

---
+++ ssg/content.py
@@ -0,0 +1,19 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data.add({"content", content})
+
